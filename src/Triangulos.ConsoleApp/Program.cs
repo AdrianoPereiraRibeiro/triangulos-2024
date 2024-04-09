c6e82ba80e056846815f1cd1f3e15b85e9dcf28d
AdrianoPereiraRibeiro <adrianoribeiro291005@gmail.com>
2024-04-09 12:47:46 -0300

    Docs

--- src/Triangulos.ConsoleApp/Program.cs
+++ src/Triangulos.ConsoleApp/Program.cs
@@ -1,10 +1,41 @@
 ﻿namespace Triangulos.ConsoleApp
 {
-    internal class Program
+    internal partial class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            TrianguloChecando tr = CapturarValores();
+            int retorno = tr.ProcessamentoIF();
+            VerificarTipoTriangulo(retorno);
+            Console.ReadLine();
+        }
+
+        public static void VerificarTipoTriangulo(int retorno)
+        {
+            switch (retorno)
+            {
+                case 0:
+                    Console.WriteLine("O triângulo é equilátero.");
+                    break;
+                case 1:
+                    Console.WriteLine("O triângulo é isósceles. ");
+                    break;
+                case 2:
+                    Console.WriteLine("O triângulo é escaleno. ");
+                    break;
+            }
+        }
+
+        public static TrianguloChecando CapturarValores()
+        {
+            TrianguloChecando tr = new TrianguloChecando();
+            Console.WriteLine("Digite o valor de X do triângulo:");
+            tr.x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite o valor de Y do triângulo:");
+            tr.y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite o valor de Z do triângulo:");
+            tr.z = Convert.ToDouble(Console.ReadLine());
+            return tr;
         }
     }
 }
---
+++ src/Triangulos.ConsoleApp/TrianguloChecando.cs
@@ -0,0 +1,25 @@
+﻿namespace Triangulos.ConsoleApp
+{
+    internal partial class Program
+    {
+        public class TrianguloChecando
+        {
+            public double x;
+            public double y;
+            public double z;
+            public int retorno;
+            public int ProcessamentoIF()
+            {
+                if (x == y || y == z) { retorno = 1; };
+                if (x == y && x == z) { retorno = 0; };
+                if (x != y && x != z && y != z) { retorno = 2; };
+
+                return retorno;
+            }
+
+
+
+
+        }
+    }
+}
